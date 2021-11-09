62940da5c368918811b74065222e654b2f9f6c18
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-09 21:21:19 +0000

    added sorting option for title & price but needs work

--- src/components/FilterOptions.jsx
+++ src/components/FilterOptions.jsx
@@ -1,15 +1,18 @@
 import { Row, FormControl } from "react-bootstrap"
 
-const FilterOptions = ({ category, amountOfBooks, handleCategoryChange, handleSortOrder, handleSearch, handleResultsPerPage}) => (
+const FilterOptions = ({ category, searchQuery, amountOfBooks, handleCategoryChange, handleSortOrder, handleSearch, handleResultsPerPage}) => (
     <>
         <Row>
             <FormControl className="mb-3 mx-3"
             placeholder="Search A Book..."
-            onChange={handleSearch}
+            onKeyUp={handleSearch}
             />
         </Row>
         <Row className="justify-content-between mb-3">
-            <h2 className="pl-3">{amountOfBooks} Latest {category} Books</h2>
+            {
+                searchQuery ? <h2 className="pl-3">{amountOfBooks} Books For {searchQuery}</h2>
+                : <h2 className="pl-3">{amountOfBooks} {category} Books</h2> 
+            }
             <div>
                 <select onChange={handleCategoryChange} name="category" id="category" className="mr-3">
                     <option value="horror">Horror</option>
@@ -19,14 +22,16 @@ const FilterOptions = ({ category, amountOfBooks, handleCategoryChange, handleSo
                     <option value="scifi">Sci-Fi</option>
                 </select>
                 <select onChange={handleSortOrder} name="sort" id="sort" className="mr-3">
-                    <option value="a-z">Sort (A-Z)</option>
-                    <option value="z-a">Sort (Z-A)</option>
-                    <option value="low-high">Sort (Price Low-High)</option>
-                    <option value="high-low">Sort (Price High-Low)</option>
+                    <option value="a-z">Title (A-Z)</option>
+                    <option value="z-a">Title (Z-A)</option>
+                    <option value="low-high">Price (Low-High)</option>
+                    <option value="high-low">Price (High-Low)</option>
                 </select>
                 <select onChange={handleResultsPerPage} name="resultsAmount" id="resultsAmount" className="mr-3">
-                    <option value="25">25</option>
-                    <option value="50">50</option>
+                    <option value="24">24</option>
+                    <option value="48">48</option>
+                    <option value="96">96</option>
+                    <option value="150">All</option>
                 </select>
             </div>
         </Row>
--- src/components/LatestBooks.jsx
+++ src/components/LatestBooks.jsx
@@ -12,11 +12,12 @@ import FilterOptions from './FilterOptions'
 const LatestBooks = () => {
     
     const [category, setCategory] = useState('Horror')
+    const [searchQuery, setSearchQuery] = useState(null)
     const [books, setBooks] = useState(horrorBooks)
-    const [amountOfBooks, setAmountOfBooks] = useState(25)
+    const [amountOfBooks, setAmountOfBooks] = useState(24)
     // const [booksOrder, setBooksOrder] = useState(horrorBooks)
 
-    const handleCategoryChange = (e) => {
+    const handleCategoryChange = e => {
         setCategory(e.target.value.charAt(0).toUpperCase() + e.target.value.substr(1))
         switch (e.target.value) {
             case 'horror':
@@ -39,18 +40,64 @@ const LatestBooks = () => {
         }
     }
 
-    const handleSortOrder = () => {
-        console.log('need to complete this')
+    const handleSortOrder = e => {
+        switch (e.target.value) {
+            case 'a-z':
+                const azBookTitles = []
+                books.forEach(book => azBookTitles.push(book.title))
+                azBookTitles.sort()
+                const azNewBookOrder = []
+                azBookTitles.forEach(bookTitle => azNewBookOrder.push(books.find(({title}) => title === bookTitle)))
+                setBooks(azNewBookOrder)
+            break;
+            case 'z-a':
+                const zaBookTitles = []
+                books.forEach(book => zaBookTitles.push(book.title))
+                zaBookTitles.sort()
+                zaBookTitles.reverse()
+                const zaNewBookOrder = []
+                zaBookTitles.forEach(bookTitle => zaNewBookOrder.push(books.find(({title}) => title === bookTitle)))
+                setBooks(zaNewBookOrder)
+            break;
+            case 'low-high':
+                const lhBookPrices = []
+                books.forEach(book => lhBookPrices.push(book.price))
+                lhBookPrices.sort()
+                console.log(lhBookPrices)
+                const lhNewBookOrder = []
+                lhBookPrices.forEach(bookPrice => lhNewBookOrder.push(books.find(({price}) => price === bookPrice)))
+                console.log(lhNewBookOrder)
+                setBooks(lhNewBookOrder)
+            break;
+            case 'high-low':
+                const hlBookPrices = []
+                books.forEach(book => hlBookPrices.push(book.price))
+                hlBookPrices.sort()
+                hlBookPrices.reverse()
+                console.log(hlBookPrices)
+                const hlNewBookOrder = []
+                hlBookPrices.forEach(bookPrice => hlNewBookOrder.push(books.find(({price}) => price === bookPrice)))
+                console.log(hlNewBookOrder)
+                setBooks(hlNewBookOrder)
+                console.log(hlNewBookOrder)
+            break;
+            default: 
+                alert('Looks like something went wrong')
+        }
     }
 
     const handleSearch = e => {
         if (e.target.value) {
             const filteredBooks = books.filter(book => book.title.toLowerCase().includes(e.target.value.toLowerCase()))
             setBooks(filteredBooks)
+            setAmountOfBooks(filteredBooks.length)
+            setSearchQuery(e.target.value)
+        } else {
+            setSearchQuery(null)
         }
     }
 
-    const handleResultsPerPage = (e) => {
+    const handleResultsPerPage = e => {
         setAmountOfBooks(e.target.value)
     }
 
@@ -58,6 +105,7 @@ const LatestBooks = () => {
         <Container>
             <FilterOptions
             category={category}
+            searchQuery={searchQuery}
             amountOfBooks={amountOfBooks} 
             handleCategoryChange={handleCategoryChange} 
             handleSortOrder={handleSortOrder} 
@@ -66,8 +114,8 @@ const LatestBooks = () => {
             />
             <Row className="justify-content-center">
                 {
-                    books.map(({asin, img}, index) => 
-                        index < amountOfBooks && <BookCover key={asin} bookImg={img} />
+                    books.map(({img}, index) => 
+                        index < amountOfBooks && <BookCover key={index} bookImg={img} />
                     )
                 }
             </Row>
