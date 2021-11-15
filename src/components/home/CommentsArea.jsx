6a85847c33fc8599c20af37b02bca84df862c711
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-15 13:14:53 +0000

    added comment area sidebar

---
+++ src/components/home/CommentsArea.jsx
@@ -0,0 +1,10 @@
+const CommentArea = () => {
+    return (
+        <>
+            <h2 className='pt-5 mt-1'>Comments</h2> 
+            <p>Select a Book to see the comments.</p>
+        </>
+    )
+}
+
+export default CommentArea
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,23 @@
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 import DisplayBooks from "./DisplayBooks"
 import Welcome from "./Welcome"
+import CommentArea from "./CommentsArea"
 
 const Home = () => (
   <>
     <Welcome />
-    <DisplayBooks />
+    <Container fluid>
+      <Row>
+        <Col md='3' className='text-center'>
+          <CommentArea />
+        </Col>
+        <Col md='9'>
+          <DisplayBooks />
+        </Col>
+      </Row>
+    </Container>
   </>
 )
 
